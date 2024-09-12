3071e9b9fab48c534bd0539aaa4777003b35e3e3
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-09-12 16:55:09 -0500

    first commit porfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet,RouterLink } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            RouterLink,
+            AboutComponent,
+            ContactComponent,
+            ExperienceComponent,
+            ProjectsComponent,
+            SkillsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+export const routes: Routes = [
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'skills',
+        component: SkillsComponent
+    },
+    {
+        path: 'experience',
+        component: ExperienceComponent
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
